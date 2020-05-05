6aed1f4a441895a4bb6ce354f294816dca5a987b
Luke Chen <luke.chen.2010@gmail.com>
2020-05-05 01:00:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0
   new_collection = []
   
-  while i < collection.length
+  while i < collection.size
     new_collection.push(yield(collection[i])
     i += 1
   end
